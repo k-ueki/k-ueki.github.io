6bc2ddaed866998516dcbbf14a805c4bec62bdf2
k-ueki <ku6.gatt@gmail.com>
2019-02-16 00:07:36 +0900

    UU

--- js/index.js
+++ js/index.js
@@ -1,4 +1,7 @@
-alert("OKOK");
-$(".hello").click(function(){
-    alert("HHHHHHH");
+$(function(){
+    $(document).on("click",".about",function(){
+        var modal_title = $(this).text();
+        $("modal-wrapper").style("display:")
+        $(".modal-title").text(modal_title);
+    })
 })
