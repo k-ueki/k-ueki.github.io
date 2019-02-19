bb7b6658b69e6e5efc48a1d6cb79e227575ea963
k-ueki <ku6.gatt@gmail.com>
2019-02-19 12:57:53 +0900

    modal

--- js/index.js
+++ js/index.js
@@ -3,7 +3,6 @@ $(function(){
         var modal_title = $(this).text();
         var st = document.getElementsByClassName("modal-wrapper").style; 
         console.log(modal_title);
-        st.display = "block";
         console.log(st);
         $(".modal-title").text(modal_title);
         alert("OK");
