89cfb012ca7e7e3f7156acf07415164f9595d0db
k-ueki <ku6.gatt@gmail.com>
2019-02-19 17:33:35 +0900

    modal

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ $(function(){
         $(".modal-wrapper").append(modal_content);
         var test = document.getElementsByClassName("modal-wrapper");
         console.log(test);
-        $(".modal-wrapper").css("display","block");
-
+        //$(".modal-wrapper").css("display","block");
+        $(".modal-wrapper").fadeIn();
     })
 })
