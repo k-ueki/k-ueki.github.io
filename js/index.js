a8fa0bde1a05cd1d83277374d2cb3e57eb5ecc9c
k-ueki <ku6.gatt@gmail.com>
2019-02-19 10:17:13 +0900

    JIS

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 $(function(){
     $(document).on("click",".about",function(){
         var modal_title = $(this).text();
-        $("modal-wrapper").style("display:")
+        $("modal-wrapper").style("display:block;")
         $(".modal-title").text(modal_title);
     })
 })
