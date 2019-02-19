26d7bdeff447b5349d68022fed9b9aefa3b9f944
k-ueki <ku6.gatt@gmail.com>
2019-02-19 13:11:47 +0900

    modal

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 $(function(){
     $(document).on("click",".about",function(){
         var modal_title = $(this).text();
-        var st = document.getElementsByClassName("modal-wrapper").style(); 
+        var st = document.getElementsByClassName("modal-wrapper"); 
         console.log(modal_title);
         console.log(st);
         $(".modal-title").text(modal_title);
