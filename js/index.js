cb461b59c4f14c9b02adecc6a29d778d0c8515c4
k-ueki <ku6.gatt@gmail.com>
2019-02-19 10:59:35 +0900

    l

--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
 $(function(){
     $(document).on("click",".about",function(){
-        var modal_title = $(this).text();
-        $("modal-wrapper").style("display:block;")
-        $(".modal-title").text(modal_title);
+        //var modal_title = $(this).text();
+        //$("modal-wrapper").style("display:block;")
+        //$(".modal-title").text(modal_title);
+        alert("OK");
     })
 })
